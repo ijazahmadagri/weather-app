7ea3c8f6d670d136e81bd0a9c3e1b811344d57a5
ijazahmadagri <ahmad.ijaz63@yahoo.com>
2025-07-17 21:56:06 +0500

    adding card

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const API_KEY = `Z9K635YUWPUZRES7FZ5BEL2MT`
 const form = document.querySelector("form")
 const search = document.querySelector("#search")
-const weather = document.querySelector("#weather")
+const card = document.querySelector(".card")
 const template = document.querySelector("#weather-template")
+const container = document.querySelector(".card-container")
+const cardcontainer = document.querySelector(".card-container")
 
 //console.log(template.querySelector('#condition_temperature').innerText)
 //template.querySelector('#condition_image').setAttribute('src', './images/clouds.png')
@@ -15,7 +17,7 @@ const getWeather = async (city) => {
     const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${API_KEY}`
     const response = await fetch(url);
     const data = await response.json()
-    
+    console.log(data)
     return showWeather(data)
 }
 
@@ -28,10 +30,18 @@ const showWeather = (data) => {
     template.querySelector('#condition_image').setAttribute('src', `./images/${data.currentConditions.icon}.png`)
     template.querySelector('#condition_temperature').innerText = `${data.currentConditions.temp} °F`;
     template.querySelector('#current_condition').innerText = `${data.currentConditions.conditions}`;
+    card.querySelector('.city').innerText = `${data.address} `;
+    card.querySelector('.state').innerText = `${data.resolvedAddress} `;
+    card.querySelector('.card-img').setAttribute('src', `./images/${data.currentConditions.icon}.png`)
+    card.querySelector('.temp-card').innerText = `${data.currentConditions.temp} °F`;
+    card.querySelector('.desc2').innerText = `${data.days[0].feelslike}°F `;
+    card.classList.remove('hide')
+
 
 
 }
 
+   
 form.addEventListener(
     "submit",
     function (event) {
